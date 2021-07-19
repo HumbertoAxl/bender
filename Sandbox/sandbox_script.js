34fc72b4eb124ce897bd788c85109c76bb87e0e5
Humberto Axl <humbertoaxl@gmail.com>
2021-07-19 15:58:33 -0300

    Julho/2021

--- Sandbox/sandbox_script.js
+++ Sandbox/sandbox_script.js
@@ -3,7 +3,6 @@
 async function pegarMarca() {
 const puppeteer = require('puppeteer');
     let marca, codigo
-    // const fs = require('fs');
     const produtos = [
         ['https://www.lojadomecanico.com.br/produto/140035/21/227/base-retangular-6-pol-para-lixadeira-ch-o-50--chiaperini-13711-'],
         ['https://www.lojadomecanico.com.br/produto/122747/2/253/cabeca-branco-em-abs-para-martelo-922-44-robust-923-44e']
