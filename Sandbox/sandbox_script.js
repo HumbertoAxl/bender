a767c427f9803de1aa07fbdd9017c0ed4192f044
Humberto Axl <humbertoaxl@gmail.com>
2021-07-20 10:09:51 -0300

    Julho/2021

--- Sandbox/sandbox_script.js
+++ Sandbox/sandbox_script.js
@@ -1,15 +1,18 @@
+const request = require("request-promise")
+const cheerio = require("cheerio")
+
+const produtos = [
+['https://www.lojadomecanico.com.br/produto/140035/21/227/base-retangular-6-pol-para-lixadeira-ch-o-50--chiaperini-13711-'],
+['https://www.lojadomecanico.com.br/produto/122747/2/253/cabeca-branco-em-abs-para-martelo-922-44-robust-923-44e']
+]
+
 async function main() {
-    require(['requirejs'], function (requirejs) {
-    });
-// var fs = requirejs("fs");
-    const request = requirejs("request-promise")
-    let produtos = [['https://www.lojadomecanico.com.br/produto/140035/21/227/base-retangular-6-pol-para-lixadeira-ch-o-50--chiaperini-13711-']
-    ['https://www.lojadomecanico.com.br/produto/122747/2/253/cabeca-branco-em-abs-para-martelo-922-44-robust-923-44e'],]
-    const cheerio = requirejs("cheerio")
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < produtos.length; i++) {
         const link = await request.get(produtos[i].toString())
         const $ = await cheerio.load(link)
         let nome = $('h1').text()
-        console.log((i + 1) + ' - ' + nome)
+        console.log((i+1) + ' - ' + nome)
     }
 }
+
+main()
