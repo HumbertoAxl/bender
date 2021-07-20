fabc03c51d382e694b1e73230dc3c022cca467b5
Humberto Axl <humbertoaxl@gmail.com>
2021-07-20 16:02:39 -0300

    1.0

--- Sandbox/produtos.js
+++
@@ -1,6 +0,0 @@
-const produtos = [
-    ['https://www.lojadomecanico.com.br/produto/140035/21/227/base-retangular-6-pol-para-lixadeira-ch-o-50--chiaperini-13711-'],
-    ['https://www.lojadomecanico.com.br/produto/122747/2/253/cabeca-branco-em-abs-para-martelo-922-44-robust-923-44e'],
-]
-
-module.exports = {produtos}
