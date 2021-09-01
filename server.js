1468ca8e7a82c06a6469dc6daabedafb4e208a98
Humberto Axl <humbertoaxl@gmail.com>
2021-09-01 15:50:54 -0300

    ok

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res){
     res.render('index.html');
 });
 
-app.get('/Sandbox/sandbox.html', function(req, res){
+app.get('/Sandbox/', function(req, res){
   res.redirect('/')
 });
 
