fcb99b5fd8b1424b5f5d420e787dcf9c02696157
Humberto Axl <humbertoaxl@gmail.com>
2021-09-01 16:44:35 -0300

    ok

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.listen(app.get('port'), function() {
 app.get('/teste', function(req, res) {
   // res.send(benderMail.send(req.params.destinatario, 'Teste 2 de email pelo site do bender', 'Email enviado pelo site do Bender!'))
   try {
-    res.send(benderMail.send('bender.ferimport@gmail.com', 'FerimportBot', 'humberto.axl@ferimport.com.br', 'teste', 'teste'))
     res.send('Email enviado com sucesso!2')
+    // res.send(benderMail.send('bender.ferimport@gmail.com', 'FerimportBot', 'humberto.axl@ferimport.com.br', 'teste', 'teste'))
     } catch (e) {
-        console.log('Senha ou email errados, tente novamente!2')
+        // console.log('Senha ou email errados, tente novamente!2')
         res.send('Senha ou email errados, tente novamente!2')
     }
 })
