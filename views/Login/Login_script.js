8925ec4960ac59496b2acd59b8f97e0b435d4d14
Humberto Axl <humbertoaxl@gmail.com>
2021-09-15 16:57:29 -0300

    ok

--- views/Login/Login_script.js
+++ views/Login/Login_script.js
@@ -2,25 +2,28 @@ let xhr = new XMLHttpRequest()
 async function Login() {
     let email = document.querySelector('#email').value
     let senha = document.querySelector('#senhaAcesso').value
-    sendMessage('Logando...', 'info', null, true)
-    xhr.send(await sendToServer(email, senha, "../../auth/"))
-    xhr.onreadystatechange = async function () {
-        if (xhr.readyState === 4) {
-            console.log(xhr.response)
-            switch (xhr.response) {
-                case 'OK':
-                    sendMessage('Login efetuado com sucesso!', 'success', 1150, true)
-                    // xhr.open("POST", "../../views/Menu/")
-                    // xhr.send(body);
-                    Cookies.set('email', email)
-                    setTimeout(() => window.location.href = './views/Menu/menu.html', 1200);
-                    break
-                case '535':
-                    sendMessage('Email ou senha incorretos!', 'error', 1500, true)
-                    break
-                case 'Externo':
-                    sendMessage('Seu email não é Ferimport!', 'error', 1500, true)
-                    break
+    if (email == "" || senha == "") {
+        sendMessage('Preencha todos os campos!', 'error', 1500, true)
+    } else {
+        sendMessage('Logando...', 'info', null, true)
+        xhr.send(await sendToServer(email, senha, "../../auth/"))
+        xhr.onreadystatechange = async function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.response)
+                switch (xhr.response) {
+                    case 'OK':
+                        sendMessage('Login efetuado com sucesso!', 'success', 1150, true)
+                        Cookies.set('email', email)
+                        Cookies.set('notFirstTime', true, { expires: 365 })
+                        setTimeout(() => window.location.href = './views/Menu/menu.html', 1200);
+                        break
+                    case '535':
+                        sendMessage('Email ou senha incorretos!', 'error', 1500, true)
+                        break
+                    case 'Externo':
+                        sendMessage('Seu email não é Ferimport!', 'error', 1500, true)
+                        break
+                }
             }
         }
     }
