bfc8d4528331f4c08435fa3df31f88ba67cff57a
Humberto Axl <humbertoaxl@gmail.com>
2021-09-20 11:19:24 -0300

    ok

--- views/Menu/menu_script.js
+++ views/Menu/menu_script.js
@@ -1,3 +1,27 @@
+const app = Vue.createApp({
+    methods: {
+        pegarNome() {
+            let nome = Cookies.get('email')
+            nome = nome.split('@')
+            nome = nome[0]
+            if (nome.includes('.') || nome.includes('_')) {
+                (nome.includes('.')) ? nome = nome.split('.') : nome = nome.split('_')
+                nome = nome[0]
+                nome = nome.charAt(0).toUpperCase() + nome.slice(1);
+                return nome
+            } else {
+                if (nome[0].length <= 15) {
+                    return nome[0]
+                } else {
+                    return 'usuário(a)'
+                }
+            }
+        }
+    }
+})
+
+app.mount('#menuHeader')
+
 let xhr = new XMLHttpRequest()
 async function goTo(url) {
     // alert('Clicado: ' + url)
@@ -11,15 +35,15 @@ async function goTo(url) {
     }
 }
 
-    async function sendToServer(email, senha, url) {
-        xhr.open("POST", './bender/auth' + url)
-        xhr.setRequestHeader("Accept", "application/json")
-        xhr.setRequestHeader("Content-Type", "application/json")
-        let data = `{
+async function sendToServer(email, senha, url) {
+    xhr.open("POST", './bender/auth' + url)
+    xhr.setRequestHeader("Accept", "application/json")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    let data = `{
         "email": "${email}",
         "senha": "${senha}",
         "data": "${pegarDia()}/${pegarMes()}/${pegarAno()}",
         "horario": "${pegarHora()}:${pegarMinutos()}"
     }`
-        return data
-    }
+    return data
+}
